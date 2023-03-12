75e6993bbefe94a64494a6128edfcc17e524ffe1
Akshay-Durgade <113453627+Akshay-Durgade@users.noreply.github.com>
2023-03-12 18:50:35 +0530

    All Mappings done in backend

--- React+BackEnd/spr-efarmer/src/main/java/com/efarmer/service/CropStatusService.java
+++ React+BackEnd/spr-efarmer/src/main/java/com/efarmer/service/CropStatusService.java
@@ -3,6 +3,7 @@ package com.efarmer.service;
 import java.util.List;
 
 import com.efarmer.model.CropStatus;
+import com.efarmer.model.Login;
 
 public interface CropStatusService
 {
@@ -12,5 +13,5 @@ public interface CropStatusService
 	Integer updateStatus(int id,String status);
 	List<CropStatus> getAllCrops(String status);
 	List<CropStatus> menuPageCrops(String status);
-	void updateCustomer(int cropID,int customerid);
+	void updateCustomer(int cropID,Login customer);
 }
